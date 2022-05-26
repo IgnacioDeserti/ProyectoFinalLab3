2c86ec3eb4b8b3df4c016b27eca787195edae3c2
IgnacioTosini <72680994+IgnacioTosini@users.noreply.github.com>
2022-05-26 19:32:04 -0300

    Prueba Zurdo

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         teclado = new Scanner(System.in);
 
-        System.out.println("Ahora todo cambioooo, ahora es t");
+        System.out.println("Ahora todo cambioooo, ahora es aaaa");
 
         teclado.close();
     }
