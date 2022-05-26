8ed82f01d629ecd4b71c0c3709a814da61873719
IgnacioDeserti <ignaciodeserti@gmail.com>
2022-05-26 18:33:51 -0300

    Primer printeo random

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         teclado = new Scanner(System.in);
-
+        System.out.println("DALE BO");
 
         teclado.close();
     }
