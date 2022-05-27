23ba8ebe630b6a9abdbe3734b610cb5cf828ad4a
IgnacioDeserti <ignaciodeserti@gmail.com>
2022-05-27 09:46:12 -0300

    Cambios main

--- src/Persona/app/Admin.java
+++ src/Persona/app/Admin.java
@@ -3,11 +3,9 @@ package Persona.app;
 import javax.swing.*;
 
 public class Admin extends Empleado implements I_MetodosPersona{
-    private String password;
 
     public Admin(String nombreYapellido, int dni, String departamento, String password) {
         super(nombreYapellido, dni, departamento);
-        this.password = password;
     }
 
 
