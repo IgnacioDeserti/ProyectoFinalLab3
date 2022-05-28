bc57116013a5387c991075bcd3b1cf7ddef5df4f
IgnacioDeserti <ignaciodeserti@gmail.com>
2022-05-27 23:38:44 -0300

    Creacion, lectura y muestra de archivo

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -1,7 +1,11 @@
 package com.app;
 
 import Persona.app.Cliente;
+import Persona.app.Usuario;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,6 +15,37 @@ public class Main {
     public static void main(String[] args) {
         teclado = new Scanner(System.in);
 
+        ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+        listaUsuarios.add(new Cliente("Ignacio Tosini", 44460448, "zurdosiete"));
+
+        String contenido = "";
+        int aux;
+
+        try(FileWriter fileWriter = new FileWriter("miArchi.txt")) {
+            fileWriter.write(listaUsuarios.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try(FileReader fileReader = new FileReader("miArchi.txt")) {
+
+            aux = fileReader.read();
+            while(aux != -1){
+                contenido = contenido + (char)aux;
+                try {
+                    aux = fileReader.read();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("CONTENIDO: " + contenido);
         
 
 
